cff2f337782c9eed7f6c7bf675d131e92a679dd1
benli <benli@LAPTOP-ERR69PGH>
2025-05-13 15:54:27 +0200

    added add, complete and delete task functions

--- todolist.py
+++ todolist.py
@@ -15,7 +15,7 @@ def show_menu():
     # TODO: edit task
     #print("Please choose an option:")
     print("1. View Tasks")
-    print("2. Add Tasks")
+    print("2. Add Task")
     print("3. Mark Task as Completed")
     print("4. Delete Task")
     print("5. Save & Exit")
@@ -35,6 +35,7 @@ class TodoList():
 
         with open(filepath, 'r') as f:
             self.tasklist = json.load(f)
+
         
     def view_tasks(self):
         """
@@ -51,6 +52,71 @@ class TodoList():
             print(f'{t+1}. {task['name']}: {status}')
 
         return
+    
+    
+    def add_task(self, task_name):
+        """
+        Add a new task to an existing To-Do list
+        """
+        new_task = {'id' : len(self.tasklist)+1,
+                    'name' : task_name,
+                    'done' : False
+                    }
+        self.tasklist.append(new_task)
+        return 
+    
+    
+    def complete_task(self, task_name):
+        """
+        Mark a task as completed
+        """
+
+        # assert that completed task exists
+        task_found = False
+        for task in self.tasklist:
+            if task['name'] == task_name:
+                completed_task = task
+                task_found = True
+                break
+            else:
+                pass
+
+        if not task_found:
+            print('Aufgabe nicht gefunden.')
+            return
+        
+        # check if task already done
+        if completed_task['done']:
+            print('Aufgabe bereits erledigt.')
+            return
+        
+        completed_task['done'] = True
+
+        return
+    
+
+    def delete_task(self, task_name):
+        """
+        Delete a task from the To-Do list
+        """
+
+        task_found = False
+        for t,task in enumerate(self.tasklist):
+            if task['name'] == task_name:
+                del self.tasklist[t]
+                task_found = True
+
+            # make sure ids remain consistent
+            self.tasklist[t]['id'] = t+1
+
+        if not task_found:
+            print('Aufgabe nicht gefunden.')
+            return
+        
+        return self.tasklist
+    
+
+        
 
 
 
